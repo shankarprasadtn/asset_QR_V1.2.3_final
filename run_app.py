e81531b5cc8866af5a8abec0d0758d5229313bcc
shankarprasadtn <shankarprasadtn@gmail.com>
2026-04-28 19:26:33 +0530

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -3,7 +3,7 @@ import threading
 from app import app
 
 def open_browser():
-    webbrowser.open("http://127.0.0.1:5000/login")
+    webbrowser.open("http://127.0.0.1:5005/login")
 
 if __name__ == "__main__":
     threading.Timer(1.5, open_browser).start()
